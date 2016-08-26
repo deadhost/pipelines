c4096fa6b307e946b557b0231dc743ad48596632
Deniss Viktorovs <Deniss.Viktorovs@4finance.com>
2016-08-26 10:30:10 +0300

    initial

---
+++ src/main/java/com/fourfinance/rnd/controller/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.fourfinance.rnd.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello, world!";
+    }
+
+}
