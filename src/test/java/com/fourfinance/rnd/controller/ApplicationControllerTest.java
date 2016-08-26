4765685cab269e1e77a4d216df5c552ba157b934
Deniss Viktorovs <Deniss.Viktorovs@4finance.com>
2016-08-26 11:49:57 +0300

    failing test added

---
+++ src/test/java/com/fourfinance/rnd/controller/ApplicationControllerTest.java
@@ -0,0 +1,13 @@
+package com.fourfinance.rnd.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationControllerTest {
+
+    @Test
+    public void testName() throws Exception {
+        assertTrue(false);
+    }
+}
